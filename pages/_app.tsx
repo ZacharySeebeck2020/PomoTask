9a60a64d33c325019bb2acbb47403e8953b6671a
Zachary Seebeck <seebeckzachary@gmail.com>
2022-04-17 12:15:38 -0400

    Add firebase starter.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
+import { app } from '../util/firebase';
 import Footer from '../components/global/footer';
 import Sidenav from '../components/global/sidenav'
 import DbProvider, { useDb } from '../context/DbProvider';
@@ -8,14 +9,19 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
+  useEffect(() => {
+    if (!app) return;
+    
+  }, [app])
+
   return (
     <>
       <DbProvider>
         <section className={router.pathname != '/' ? 'flex min-h-screen ml-16' : 'flex min-h-screen ml-16 '}>
-          <Sidenav/>
+          <Sidenav />
           <div className="z-30 bg-background w-full text-white">
             <Component {...pageProps} />
-            {router.pathname != '/' ? <Footer/> : ''}
+            {router.pathname != '/' ? <Footer /> : ''}
           </div>
         </section>
       </DbProvider>
